6c3dabcb4fe9e2edf10c0252d2319d532aab6505
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-05-21 09:35:59 +0000

    Funcionalidad OK, mejorar diseño y botones

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -9,73 +10,108 @@ export const Home = () => {
           <div className="container">
             <h1 className="display-4">PICKATEAM</h1>
             <p className="lead">
-              PICKATEAM Es una app/web con la que puedes encontrar personas para
-              hacer deporte, en la que se permite crear eventos o quedadas y
-              permitir que otros usuarios puedan ser informados de los eventos
-              creados y tengan la opción de, a través de un boton, informar al
-              resto de que van a asistir. Como usuario puedes tanto crear como
-              asistir a otros ya creados. La app/web presenta estadisticas e
-              historial de datos relacionados con Strava, para deportes como
-              ciclismo y running, de modo que puedas comparar con otros usuarios
-              sus registros. Pensada para deportes en pistas públicas (o incluso
-              privadas si los asistentes se ponen de acuerdo). Deportes como
-              baloncesto, futbol, volleyball playa, ciclismo y running.
+              <h2 style={{ color: "gray" }}>
+                En Pickateam queremos crear una comunidad de deportistas, a
+                través de la cual ayudarte a encontrar nuevas personas, nuevos
+                jugadores y, por qué no, nuevos equipos.
+              </h2>
             </p>
           </div>
         </div>
       </div>
-      <div
-        id="carouselExampleControls"
-        className="carousel slide "
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner rounded-3">
-          <div className="carousel-item active">
-            <img
-              src="https://pix10.agoda.net/geo/city/5270/1_5270_02.jpg?ca=6&ce=1&s=1920x822"
-              className="d-block w-100 img-fluid"
-              alt="..."
-            />
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col">
+            <div
+              className="card"
+              style={{ width: "300px", marginBottom: "10px" }}
+            >
+              <img
+                className="card-img-top"
+                src="https://placebeard.it/640x360"
+                alt="This is an image"
+              />
+              <div className="card-body">
+                <h5 className="card-title">BUSCA TU CIUDAD</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="carousel-item">
-            <img
-              src="https://images.musement.com/cover/0002/45/barcelona-jpg_header-144696.jpeg?w=1200&h=630&q=95&fit=crop"
-              className="d-block w-100"
-              alt="..."
-            />
+          <div className="col">
+            <div
+              className="card"
+              style={{ width: "300px", marginBottom: "10px" }}
+            >
+              <img
+                className="card-img-top"
+                src="https://placebeard.it/640x360"
+                alt="This is an image"
+              />
+              <div className="card-body">
+                <h5 className="card-title">ENCUENTRA TU LUGAR</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="carousel-item ">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/8/8c/The_whole_Alhambra_Granada_Spain.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
+          <div className="col">
+            <div
+              className="card"
+              style={{ width: "300px", marginBottom: "10px" }}
+            >
+              <img
+                className="card-img-top"
+                src="https://placebeard.it/640x360"
+                alt="This is an image"
+              />
+              <div className="card-body">
+                <h5 className="card-title">CREA TU EVENTO</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="card"
+              style={{ width: "300px", marginBottom: "10px" }}
+            >
+              <img
+                className="card-img-top"
+                src="https://placebeard.it/640x360"
+                alt="This is an image"
+              />
+              <div className="card-body">
+                <h5 className="card-title">EMPIEZA A JUGAR</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+      </div>
+      <div>
+        <div className="d-grid gap-2 col-6 mx-auto mt-5 mb-5">
+          <Link to="/register">
+            <button className="btn btn-warning text-white" type="button">
+              REGISTRATE
+            </button>
+          </Link>
+          <Link to="/login">
+            <button className="btn btn-warning text-white" type="button">
+              LOGIN
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
