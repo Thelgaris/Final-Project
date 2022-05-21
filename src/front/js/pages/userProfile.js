d803eefac2ff95b2db2fa1cef3944ccb500af930
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-05-21 16:25:46 +0000

    ..

--- src/front/js/pages/userProfile.js
+++ src/front/js/pages/userProfile.js
@@ -66,14 +66,49 @@ export const UserProfile = () => {
         />
       </div>
 
-      <div className="input-group mx-auto mb-3 w-25">
-        <input
-          type="text"
-          className="form-control text-center"
-          placeholder="Deportes que practicas"
-          aria-label="Apellidos"
-          aria-describedby="basic-addon1"
-        />
+      <button
+        type="button"
+        className="btn btn-light w-25"
+        data-bs-toggle="modal "
+        data-bs-target="#exampleModal"
+      >
+        Deportes que practicas
+      </button>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
