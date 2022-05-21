85d42e8e430e152c9a591dafe67d544c9fa59e6d
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-05-21 09:59:14 +0000

    hecho el responsive

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,30 +7,28 @@ export const Home = () => {
     <div className="text-center mt-5">
       <div className="row justify-content-center">
         <div className="jumbotron jumbotron-fluid w-50">
-          <div className="container">
+          <div className="container-fluid">
             <h1 className="display-4">PICKATEAM</h1>
-            <p className="lead">
-              <h2 style={{ color: "gray" }}>
-                En Pickateam queremos crear una comunidad de deportistas, a
-                través de la cual ayudarte a encontrar nuevas personas, nuevos
-                jugadores y, por qué no, nuevos equipos.
-              </h2>
-            </p>
+            <h2 style={{ color: "gray" }}>
+              En Pickateam queremos crear una comunidad de deportistas, a través
+              de la cual ayudarte a encontrar nuevas personas, nuevos jugadores
+              y, por qué no, nuevos equipos.
+            </h2>
           </div>
         </div>
       </div>
-      <div className="container mt-4">
+      <div className="container-fluid d-flex justify-content-center mt-4">
         <div className="row">
-          <div className="col">
-            <div
-              className="card"
-              style={{ width: "300px", marginBottom: "10px" }}
-            >
-              <img
-                className="card-img-top"
-                src="https://placebeard.it/640x360"
-                alt="This is an image"
-              />
+          <div
+            className="card"
+            style={{ width: "300px", marginBottom: "10px" }}
+          >
+            <img
+              className="card-img-top"
+              src="https://placebeard.it/640x360"
+              alt="This is an image"
+            />
+            <div className="container-fluid">
               <div className="card-body">
                 <h5 className="card-title">BUSCA TU CIUDAD</h5>
                 <p className="card-text">
@@ -100,14 +98,14 @@ export const Home = () => {
         </div>
       </div>
       <div>
-        <div className="d-grid gap-2 col-6 mx-auto mt-5 mb-5">
+        <div className="d-grid gap-2 col-6-sm mx-auto mt-5 mb-5">
           <Link to="/register">
-            <button className="btn btn-warning text-white" type="button">
+            <button className="btn btn-warning btn-sm text-white" type="button">
               REGISTRATE
             </button>
           </Link>
           <Link to="/login">
-            <button className="btn btn-warning text-white" type="button">
+            <button className="btn btn-warning btn-sm text-white" type="button">
               LOGIN
             </button>
           </Link>
