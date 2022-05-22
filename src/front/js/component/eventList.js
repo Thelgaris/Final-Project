12d2865e708167b09f64b1d6dc816323e1151382
Thelgaris <alex.gabarron84@gmail.com>
2022-05-22 10:22:45 +0000

    workingONIt

--- src/front/js/component/eventList.js
+++ src/front/js/component/eventList.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { InfoEvent } from "./infoEvent";
 
 export const EventList = () => {
   return (
     <div className="container row">
       <ul className="list-group list-group-flush row text-center">
         <div className="col d-flex ">
-          <li className="list-group-item w-50 ">Evento 1</li>
-          <li className="list-group-item w-50 ">Evento 2</li>
+          <li className="list-group-item w-50 ">{InfoEvent}</li>
+          <button
+            className="btn btn-primary list-group-item w-50 "
+            onClick={() => {
+              {
+                InfoEvent();
+              }
+            }}
+          >
+            {InfoEvent}
+          </button>
         </div>
         <div className="col d-flex ">
           <li className="list-group-item w-50 ">Evento 3</li>
