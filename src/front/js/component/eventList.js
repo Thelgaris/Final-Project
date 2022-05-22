b44052823570e0c204884a0fdfc2787bdd004e79
Thelgaris <alex.gabarron84@gmail.com>
2022-05-22 19:47:38 +0000

    working

--- src/front/js/component/eventList.js
+++ src/front/js/component/eventList.js
@@ -4,19 +4,17 @@ import { InfoEvent } from "./infoEvent";
 
 export const EventList = () => {
   return (
-    <div className="container row">
+    <div className="container row ">
       <ul className="list-group list-group-flush row text-center">
         <div className="col d-flex ">
-          <li className="list-group-item w-50 ">{InfoEvent}</li>
+          <li className="list-group-item w-50 ">Evento 1</li>
           <button
-            className="btn btn-primary list-group-item w-50 "
+            className="btn btn-link list-group-item w-50 "
             onClick={() => {
-              {
-                InfoEvent();
-              }
+              <InfoEvent />;
             }}
           >
-            {InfoEvent}
+            Evento 2
           </button>
         </div>
         <div className="col d-flex ">
