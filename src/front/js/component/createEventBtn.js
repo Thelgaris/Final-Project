86e75b7b21976de26c6ea315547fe34dbe4542d0
Thelgaris <alex.gabarron84@gmail.com>
2022-05-26 18:12:57 +0000

    testing

--- src/front/js/component/createEventBtn.js
+++ src/front/js/component/createEventBtn.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const createEventBtn = () => {
+export const CreateEventBtn = () => {
   return (
     <div>
       <button
         type="button"
-        class="btn btn-warning"
+        className="btn btn-warning"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
       >
@@ -14,36 +14,125 @@ export const createEventBtn = () => {
       </button>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="exampleModal"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <input
+                type="text"
+                aria-label="Nombre"
+                placeholder="Nombre del Evento"
+                className="input-group mb-3 border-0"
+              />
+
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
-            <div class="modal-footer">
+            <div className="modal-body">
+              <div className="input-group mb-3">
+                <label
+                  className="input-group-text w-25"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Ciudad
+                </label>
+                <select className="form-select" id="Ciudad">
+                  <option selected></option>
+                  <option value="1">Barcelona</option>
+                  <option value="2">Cadiz</option>
+                  <option value="3">Granada</option>
+                </select>
+              </div>
+              <div className="input-group mb-3">
+                <label
+                  className="input-group-text w-25"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Pistas
+                </label>
+                <select className="form-select" id="Pistas">
+                  <option selected></option>
+                  <option value="1">Pista 1</option>
+                  <option value="2">Pista 2</option>
+                  <option value="3">Pista 3</option>
+                  <option value="3">...</option>
+                  <option value="3">Punto de encuentro</option>
+                </select>
+              </div>
+
+              <div className="input-group mb-3">
+                <label
+                  className="input-group-text w-25"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Deporte
+                </label>
+                <select className="form-select" id="Deporte">
+                  <option selected></option>
+                  <option value="1">Running</option>
+                  <option value="2">Ciclismo</option>
+                  <option value="3">Futbol</option>
+                  <option value="3">Basket</option>
+                  <option value="3">Otro</option>
+                </select>
+              </div>
+              <div className="input-group mb-3">
+                <label
+                  className="input-group-text w-25"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Fecha
+                </label>
+                <select className="form-select" id="Fecha">
+                  <option selected></option>
+                  <option value="1"></option>
+                  <option value="2"></option>
+                </select>
+              </div>
+              <div className="input-group mb-3">
+                <label
+                  className="input-group-text w-25"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Hora
+                </label>
+                <select className="form-select" id="Hora">
+                  <option selected></option>
+                  <option value="1"></option>
+                  <option value="2"></option>
+                </select>
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="file"
+                  className="form-control w-25"
+                  id="inputGroupFile01"
+                />
+              </div>
+              <div className="mb-3">
+                <textarea
+                  className="form-control"
+                  id="descripcion"
+                  rows="3"
+                  placeholder="Describe tu evento"
+                ></textarea>
+              </div>
+            </div>
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
+                className="btn btn-warning w-50 content-center"
               >
-                Close
-              </button>
-              <button type="button" class="btn btn-primary">
-                Save changes
+                Crear
               </button>
             </div>
           </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,9 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
+import { EventsComponent } from "../component/eventsComponent";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return <div className="text-center mt-5"></div>;
+  return (
+    <div className="text-center mt-5">
+      <EventsComponent />
+    </div>
+  );
 };
