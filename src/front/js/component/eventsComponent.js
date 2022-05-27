78c0d7f8c6fedf43ccc2b0113f324035ce8306e0
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-05-27 09:41:59 +0000

    add event to pill

--- src/front/js/component/eventsComponent.js
+++ src/front/js/component/eventsComponent.js
@@ -5,18 +5,20 @@ import { CreateEventBtn } from "./createEventBtn";
 export const EventsComponent = () => {
   return (
     <div className="container">
-      <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">Próximos Eventos</h5>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Evento 1</li>
-          <li className="list-group-item">Evento 2</li>
-          <li className="list-group-item">Evento 3</li>
-        </ul>
-        <div className="card-footer justify-content-center">
-          <div href="#" className="card-link ">
-            <CreateEventBtn />
+      <div className="justify-content-center">
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Próximos Eventos</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Evento 1</li>
+            <li className="list-group-item">Evento 2</li>
+            <li className="list-group-item">Evento 3</li>
+          </ul>
+          <div className="card-footer justify-content-center">
+            <div href="#" className="card-link ">
+              <CreateEventBtn />
+            </div>
           </div>
         </div>
       </div>
