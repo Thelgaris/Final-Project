bb4067b6ee8dc16fd723370a7c2510a353a133de
Thelgaris <alex.gabarron84@gmail.com>
2022-05-28 14:29:08 +0000

    mergingcomponents

--- src/front/js/component/eventList.js
+++ src/front/js/component/eventList.js
@@ -6,15 +6,8 @@ export const EventList = () => {
   return (
     // Hay que hacer un map de la tabla de pistas
     <div className="container row justify-content-center">
-      <div className="list-group col-4">
-        <p
-          className="list-group-item list-group-item-action"
-          onClick={() => {
-            <eventInfo />;
-          }}
-        >
-          Nombre Evento 1
-        </p>
+      <div className="list-group col-4 offset-4">
+        <EventList />
       </div>
     </div>
   );
