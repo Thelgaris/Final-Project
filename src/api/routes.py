b9d48dc4e595cd5d8890948b90a4ba682831e0a6
Thelgaris <alex.gabarron84@gmail.com>
2022-05-29 09:28:06 +0000

    merged

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,46 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity,jwt_required
 
 api = Blueprint('api', __name__)
 
+@api.route('/login',methods=['POST'])
+def login_user():
+    body_email = request.json.get('email')
+    body_password = request.json.get('password')
+    if body_email and body_password:
+        user = User.query.filter_by(email=body_email).filter_by(password=body_password).first()
+        if user:
+            access_token = create_access_token(identity=user.id)
+            return jsonify({"logged": True, "user": user.serialize(), "access_token": access_token}),200
+        else:
+             return jsonify({"logged": False, "msg": "Información incorrecta"}), 400
+    else: 
+        return jsonify({"logged": False, "msg": "Información incorrecta"}), 400
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/register', methods=['POST'])
+def Register_user():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    body_name = request.json.get("name")
+    body_birth = request.json.get("birth")
+    new_user = User(email=body_email, password=body_password, name=body_name, birth=body_birth)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"User": new_user.serialize()}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/userProfile', methods=['POST'])
+def user_details():
+    body_email = request.json.get("email") 
+    body_name = request.json.get("name")
+    body_surname = request.json.get("surname")
+    body_birth = request.json.get("birth")
+    body_gender = request.json.get("gender")
+    body_city = request.json.get("city")
+    
+    new_user = User(email=body_email,  name=body_name, surname=body_surname, birth=body_birth, gender=body_gender, city=body_city)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"User": new_user.serialize()}), 200
---
+++ src/front/js/pages/homepageafterlogin.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "../../styles/homepageafterlogin.css";
+import { EventsComponent } from "../component/eventsComponent";
+import { ProfileComp } from "../component/profileComp";
+import { StravaData } from "../component/stravaData";
+import { PistaList } from "../component/pistaList";
+import { EventList } from "../component/eventList";
+import { EventInfo } from "../component/eventInfo";
+import { PistaInfo } from "../component/pistaInfo";
+
+export const Homepageafterlogin = () => {
+  return (
+    <div className="text-center mt-5">
+      <div className="row justify-content-center">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container-fluid row">
+            <div className="col-3 d-inline">
+              <ProfileComp />
+            </div>
+            <div className="col-6">
+              <h1 className="">PICKATEAM</h1>
+              <div className="d-flex ">
+                <h2 style={{ color: "gray" }}>
+                  En Pickateam queremos crear una comunidad de deportistas, a
+                  través de la cual ayudarte a encontrar nuevas personas, nuevos
+                  jugadores y, por qué no, nuevos equipos.
+                </h2>
+              </div>
+            </div>
+            <div className="col-2 mt-4">
+              <EventsComponent />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="button-container container-fluid row d-flex">
+        <div className="col-3 mt-4 d-grid">
+          <StravaData />
+        </div>
+        <div className="col-6 ">
+          <img
+            className="city mt-4 mb-4 w-100 rounded"
+            src="https://placebeard.it/640x360"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="row">
+        <ul
+          className="nav nav-pills  d-flex justify-content-center "
+          role="tablist"
+        >
+          <li className="nav-item col-2" role="presentation">
+            <button
+              className="nav-link btn  btn-bg-light w-100 text-black"
+              id="pistas"
+              data-bs-toggle="pill"
+              data-bs-target="#pistasList"
+              type="button"
+              role="tab"
+              aria-controls="pills-home"
+              aria-selected="true"
+              onClick={() => {}}
+            >
+              Pistas
+            </button>
+          </li>
+          <li className="nav-item col-2 " role="presentation">
+            <button
+              type="button "
+              className="nav-link btn btn-bg-light w-100 text-black"
+              id="eventos"
+              data-bs-toggle="pill"
+              data-bs-target="#eventosList"
+              role="tab"
+              aria-controls="pills-profile"
+              aria-selected="false"
+              onClick={() => {}}
+            >
+              Eventos
+            </button>
+          </li>
+        </ul>
+        <div className="tab-content row " id="pills-tabContent">
+          <div
+            className="tab-pane fade show "
+            id="pistasList"
+            role="tabpanel"
+            aria-labelledby="pills-home-tab"
+          >
+            <PistaInfo />
+          </div>
+          <div
+            className="tab-pane fade "
+            id="eventosList"
+            role="tabpanel"
+            aria-labelledby="pills-profile-tab"
+          >
+            <EventInfo />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
