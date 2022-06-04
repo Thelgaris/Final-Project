040ca256af6f65e944c9f57ffc9a7030fd527e65
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-06-04 17:21:31 +0000

    ..

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,12 +2,9 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       sports: [],
-<<<<<<< HEAD
       userSports: [],
       user_id: null,
       logged: null,
-=======
->>>>>>> da9d1dc2e0aab9f2b3fd5ac58ba83234c790de56
       url: "https://3001-thelgaris-finalproject-xgsiog3kl72.ws-eu46.gitpod.io/api",
     },
     actions: {
