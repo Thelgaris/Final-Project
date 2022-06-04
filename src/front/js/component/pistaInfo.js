fbe3770f5b6842f9bafb2a4e7ac16c25fe6bb4b6
Thelgaris <alex.gabarron84@gmail.com>
2022-06-04 19:18:03 +0000

    pistasCasiOk

--- src/front/js/component/pistaInfo.js
+++ src/front/js/component/pistaInfo.js
@@ -4,6 +4,7 @@ import { Context } from "../store/appContext";
 
 export const PistaInfo = () => {
   const { store, actions } = useContext(Context);
+
   useEffect(() => {
     actions.getPistas();
   }, []);
@@ -11,58 +12,61 @@ export const PistaInfo = () => {
   return (
     <div className="container pistaInfo">
       <div className="row d-flex justfy-content-center">
-        <div className="col-4">
-          <button
-            type="button"
-            className="btn btn-link text-decoration-none text-dark"
-            data-bs-toggle="modal"
-            data-bs-target="#pista"
-          >
-            Nombre pista
-          </button>
-        </div>
+        {store.pistas.map((pista) => {
+          return (
+            <div className="col-4 " key={pista.id}>
+              <button
+                type="button"
+                className="btn btn-link text-decoration-none text-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#pista"
+              >
+                {pista.name}
+              </button>
+              <div
+                className="modal fade"
+                id="pista"
+                tabIndex="-1"
+                aria-labelledby="pistaLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header bg-light bg-gradient">
+                      <h5 className="modal-title">{pista.name}</h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body ">
+                      <div className="card w-100" style={{ width: "18rem" }}>
+                        <img
+                          src="https://picsum.photos/seed/picsum/300/200"
+                          className="card-img-top"
+                          alt="ImagenPista"
+                        />
+                        <div className="card-body text-center">
+                          <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Ciudad</li>
 
-        <div
-          className="modal fade"
-          id="pista"
-          tabIndex="-1"
-          aria-labelledby="pistaLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header bg-light bg-gradient">
-                <h5 className="modal-title" id="pistaLabel">
-                  Nombre Pista
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body ">
-                <div className="card w-100" style={{ width: "18rem" }}>
-                  <img
-                    src="https://picsum.photos/seed/picsum/300/200"
-                    className="card-img-top"
-                    alt="ImagenPista"
-                  />
-                  <div className="card-body text-center">
-                    <ul className="list-group list-group-flush">
-                      <li className="list-group-item">Ciudad</li>
+                            <li className="list-group-item">{pista.addres}</li>
 
-                      <li className="list-group-item">Dirección</li>
-
-                      <li className="list-group-item">Descripción</li>
-                    </ul>
+                            <li className="list-group-item">
+                              {pista.description}
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
