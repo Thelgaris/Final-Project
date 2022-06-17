82b5d1cd608f903546ee851fbe4e04cd2cc02bb1
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-06-17 17:55:47 +0000

    merge

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -70,25 +70,6 @@ export const Footer = () => (
           4GeeksAcademy
         </a>
       </div>
-=======
-
-  <footer className="bg-light sticky-bottom">
-    <div className="container p-2">
-      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-        <Link to="/nuestraWeb" className="text-decoration-none text-dark">
-          <li className="nav-item px-2">Cómo funciona PICKATEAM</li>
-        </Link>
-        <Link to="/nuestraWeb" className="text-decoration-none text-dark">
-          <li className="nav-item px-2">Nuestra web</li>
-        </Link>
-        <Link to="/nuestraWeb" className="text-decoration-none text-dark">
-          <li className="nav-item px-2">Quiénes somos</li>
-        </Link>
-        <Link to="/nuestraWeb" className="text-decoration-none text-dark">
-          <li className="nav-item px-2">Contacta con nosotros</li>
-        </Link>
-      </ul>
-      <p className="text-center text-muted">2022 PICKATEAM</p>
     </div>
   </footer>
 );
