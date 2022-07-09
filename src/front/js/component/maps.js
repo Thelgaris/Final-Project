6204231077dacbc196c2c162ecaf3f4b95fe27e3
Jose Manuel <jose.ma.mm.91@gmail.com>
2022-07-09 10:48:22 +0000

    Establecidas coordenadas de pistas (prueba), funciona bien. Centrado del mapa al seleccionar ciduad de usuario: correcto, añadido city.name para modal pistasInfo

--- src/front/js/component/maps.js
+++ src/front/js/component/maps.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import {
@@ -15,6 +14,29 @@ import {
   geocodeByPlaceId,
   getLatLng,
 } from "react-places-autocomplete";
+
+const markers = [
+  {
+    id: 1,
+    name: "Pabellón Centro Histórico Cádiz",
+    position: { lat: 36.5354136382845, lng: -6.303692724427368 },
+  },
+  {
+    id: 2,
+    name: "El Náutico",
+    position: { lat: 36.53697371987197, lng: -6.291498488605275 },
+  },
+  {
+    id: 3,
+    name: "Playa de La Caleta",
+    position: { lat: 36.529961107451825, lng: -6.305780270565639 },
+  },
+  {
+    id: 4,
+    name: "Polideportivo La Mirandilla, Campo del Sur",
+    position: { lat: 36.52763867174823, lng: -6.292638137754715 },
+  },
+];
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -43,17 +65,50 @@ const Map = () => {
   const [lat, setLat] = useState(40.4303759999059);
   const [lng, setLng] = useState(-3.7049425337888837);
   const center = { lat, lng };
+  const [activeMarker, setActiveMarker] = useState(null);
+
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
+
+  const handleOnLoad = (map) => {
+    const bounds = new google.maps.LatLngBounds();
+    markers.forEach(({ position }) => bounds.extend(position));
+    map.fitBounds(bounds);
+  };
 
   return (
     <div className="container">
-      <SearchPlaces setLat={setLat} setLng={setLng} />
+      <div className="searchBox">
+        <SearchPlaces setLat={setLat} setLng={setLng} />
+      </div>
       <GoogleMap
-        zoom={8}
+        zoom={10}
         center={center}
         mapContainerStyle={mapContainerStyle}
         options={options}
+        onLoad={handleOnLoad}
+        onClick={() => setActiveMarker(null)}
       >
-        <Marker position={{ lat, lng }} onClick={() => {}} />
+        {markers.map(({ id, name, position }) => (
+          <Marker
+            key={id}
+            center={position}
+            position={position}
+            onClick={() => {
+              handleActiveMarker(id);
+            }}
+          >
+            {activeMarker === id ? (
+              <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                <div>{name}</div>
+              </InfoWindow>
+            ) : null}
+          </Marker>
+        ))}
       </GoogleMap>
     </div>
   );
@@ -128,4 +183,3 @@ export const SearchPlaces = ({ setLat, setLng, setCity }) => {
     </div>
   );
 };
-
